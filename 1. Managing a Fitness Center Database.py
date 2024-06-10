3d092e939175a5e5ff1fa5169d1d6c5716756adb
Marko <markogidej@yahoo.com>
2024-06-10 15:12:13 -0400

    Init Commit

---
+++ 1. Managing a Fitness Center Database.py
@@ -0,0 +1,5 @@
+# Task 1: Setting Up the Flask Environment and Database Connection
+
+# Task 2: Implementing CRUD Operations for Members
+
+# Task 3: Managing Workout Sessions
---
+++ 2. Advanced Data Querying in a Fitness Center Database.py
@@ -0,0 +1,5 @@
+# Task 1: SQL DISTINCT Usage
+
+# Task 2: SQL COUNT Functionality
+
+# Task 3: SQL BETWEEN Usage
